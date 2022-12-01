cf71c5b668277413be810841ff3354e58ce35cbf
Mihai Ungureanu <mihai2000.ungureanu@gmail.com>
2022-12-01 18:02:53 +0200

    Paralelised read from files

--- binarization.py
+++ binarization.py
@@ -7,6 +7,7 @@ class Node:
     def __init__(self):
         pass
 
+
 class Threshold(Node):
     _val: float
 
@@ -23,7 +24,10 @@ class Threshold(Node):
     def __str__(self):
         return self._val.__str__()
 
+
 """ TODO: adauga/scoate operatii in acealasi stil ca astea existente"""
+
+
 class Add(Node):
     _val1: Node
     _val2: Node
@@ -34,9 +38,10 @@ class Add(Node):
         self._val2 = val2
 
     def __str__(self):
-        return "ADD(" + self._val1.__str__() + ", " + self._val2.__str__() + ")" # reprezentare lininara
-        # return "  ADD\n" + "  /  \\ \n" + self._val1.__str__() + "   " + self._val2.__str__() + "\n" # ba desenatul asta nu mi-a iesit facem alta functie de desenare frumos
-    
+        return "ADD(" + self._val1.__str__() + ", " + self._val2.__str__() + ")"  # reprezentare lininara
+        # return "  ADD\n" + "  /  \\ \n" + self._val1.__str__() + "   " + self._val2.__str__() + "\n" # ba desenatul
+        # asta nu mi-a iesit facem alta functie de desenare frumos
+
     def set_val1(self, val):
         self._val1 = val
 
@@ -48,7 +53,8 @@ class Add(Node):
 
     def get_val2(self):
         return self._val2
-    
+
+
 class Divide(Node):
     _val1: Node
     _val2: Node
@@ -61,6 +67,7 @@ class Divide(Node):
     def __str__(self):
         return "DIV(" + self._val1.__str__() + ", " + self._val2.__str__() + ")"
         # return "  DIV\n" + "  /  \\ \n" + self._val1.__str__() + "   " + self._val2.__str__() + "\n"
+
     def set_val1(self, val):
         self._val1 = val
 
@@ -72,6 +79,7 @@ class Divide(Node):
 
     def get_val2(self):
         return self._val2
+
 
 class Multiply(Node):
     _val1: Node
@@ -85,6 +93,7 @@ class Multiply(Node):
     def __str__(self):
         return "MUL(" + self._val1.__str__() + ", " + self._val2.__str__() + ")"
         # return "  MUL\n" + "  /  \\ \n" + self._val1.__str__() + "   " + self._val2.__str__() + "\n"
+
     def set_val1(self, val):
         self._val1 = val
 
@@ -97,9 +106,12 @@ class Multiply(Node):
     def get_val2(self):
         return self._val2
 
+
 """Mi-e frica ca asta o sa faca evaluarea sa dea cu minus
 Oh well anyway
 """
+
+
 class Subtract(Node):
     _val1: Node
     _val2: Node
@@ -111,19 +123,21 @@ class Subtract(Node):
 
     def __str__(self):
         return "SUB(" + self._val1.__str__() + ", " + self._val2.__str__() + ")"
-        # return "  SUB\n" + "  /  \\ \n" + self._val1.__str__() + "   " + self._val2.__str__() + "\n" # ba desenatul asta nu mi-a iesit facem alta functie de desenare frumos
-    
+        # return "  SUB\n" + "  /  \\ \n" + self._val1.__str__() + "   " + self._val2.__str__() + "\n" # ba desenatul
+        # asta nu mi-a iesit facem alta functie de desenare frumos
+
     def set_val1(self, val):
         self._val1 = val
 
     def set_val2(self, val):
         self._val2 = val
-    
+
     def get_val1(self):
         return self._val1
 
     def get_val2(self):
         return self._val2
+
 
 def build_mock_tree(thresholds):
     # model de alegere operatie random:
@@ -132,10 +146,10 @@ def build_mock_tree(thresholds):
     # if op == "ADD":
     # return Add(Threshold(t1), Threshold(t2))
     return Add(
-            Multiply(Add(Multiply(thresholds[0], thresholds[1]), Add(thresholds[2], thresholds[3])),
-                        Multiply(Subtract (thresholds[4], thresholds[5]), Add(thresholds[6], thresholds[7])))
-            ,Subtract(Multiply(Add(thresholds[8], thresholds[9]), Multiply(thresholds[10],thresholds[11])),
-                      Subtract(Add(thresholds[12], thresholds[13]), Add(thresholds[14], thresholds[15]))))
+        Multiply(Add(Multiply(thresholds[0], thresholds[1]), Add(thresholds[2], thresholds[3])),
+                 Multiply(Subtract(thresholds[4], thresholds[5]), Add(thresholds[6], thresholds[7])))
+        , Subtract(Multiply(Add(thresholds[8], thresholds[9]), Multiply(thresholds[10], thresholds[11])),
+                   Subtract(Add(thresholds[12], thresholds[13]), Add(thresholds[14], thresholds[15]))))
 
 
 def parse_input(global_filename):
