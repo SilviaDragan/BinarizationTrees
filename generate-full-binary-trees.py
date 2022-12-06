9ad62c0893319ceba732e9bfc8290dbf69e37006
Mihai00-roacs <56954718+Mihai00-roacs@users.noreply.github.com>
2022-12-07 01:12:55 +0200

    Add files via upload

--- generate-full-binary-trees.py
+++ generate-full-binary-trees.py
@@ -1,14 +1,47 @@
 import os
+from asyncio import Lock
+from enum import Enum
+import random
+import sys
+import math
 
+from binarization import Add, Multiply, Subtract, Divide, Threshold, parse_input
 from file_loader import FileLoader
+from tree_evaluation import evaluate_tree
+
+N = 1  # numarul de generari random
+MIN_F_MEASURE = 90  # scorul minim pentru ca un tree sa fie considerat valid
+lock = Lock()
+thr_f_measure = []
 
 
-# Function that iterates through all files from MPS-global folder
+class Operators(Enum):
+    ADD = 1
+    MULTIPLY = 2
+    SUBTRACT = 3
+    DIVIDE = 4
+
+
+ops_list = [Operators.ADD, Operators.MULTIPLY, Operators.SUBTRACT, Operators.DIVIDE];
+
+
+def get_node(op):
+    match op:
+        case Operators.ADD:
+            return Add(None, None)
+        case Operators.MULTIPLY:
+            return Multiply(None, None)
+        case Operators.SUBTRACT:
+            return Subtract(None, None)
+        case Operators.DIVIDE:
+            return Divide(None, None)
+
+
 def iterate_through_files():
     ths = []
     for filename in os.listdir("MPS-global"):
         if filename.endswith(".CSV"):
-            th = FileLoader("MPS-Global/" + filename)
+            th = FileLoader("MPS-Global/" + filename, thr_f_measure)
             th.start()
             ths.append(th)
     # wait for all threads to finish
@@ -17,8 +50,12 @@ def iterate_through_files():
 
 
 def main():
+    # TODO: script prin care rulam codul cu toate fisierele pe rand
     iterate_through_files()
+    for thresholds, f_measures in thr_f_measure:
+        print("thresholds: " + str(thresholds) + " f_measures: " + str(f_measures))
 
 
 if __name__ == '__main__':
+    # ca sa rulati adaugati ca parametri din Edit Configurations: MPS-Global/[AVE_INT] 2_1.CSV (sau oricare alt fisier)
     main()
