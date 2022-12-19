090a4f1e07b77b674b5766a86d4296eeec84e8f7
SilviaDragan <silvia_dragan@yahoo.com>
2022-12-19 22:27:25 +0200

    write best trees (from training set) to output files

--- src/global_binarization/file_loader.py
+++ src/global_binarization/file_loader.py
@@ -1,16 +1,36 @@
 import os
 import sys
+
 sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
-from copy import deepcopy
 import threading
-from commons.tree_generation import generate_tree, populate_tree_with_thresholds
+
 lock = threading.Lock()
+
 
 def iterate_through_files():
     ths = []
     thr_f_measure = []
-    
+
     for filename in os.listdir("MPS-Global"):
+        if filename.endswith(".CSV"):
+            # trebuie sa clonam tree ul la fiecare folosire, si sa refacem si popularea sa nu mai tina cont de coada
+            th = FileLoader("MPS-Global/" + filename, thr_f_measure)
+            th.start()
+            ths.append(th)
+
+    # wait for all threads to finish
+    for th in ths:
+        th.join()
+    return thr_f_measure
+
+def get_training_files():
+    ths = []
+    thr_f_measure = []
+
+    with open("train_file.txt", 'r') as f_in:
+        train_files = [line.rstrip() for line in f_in]
+
+    for filename in train_files:
         if filename.endswith(".CSV"):
             # trebuie sa clonam tree ul la fiecare folosire, si sa refacem si popularea sa nu mai tina cont de coada
             th = FileLoader("MPS-Global/" + filename, thr_f_measure)
@@ -30,11 +50,12 @@ def parse_input(global_filename):
 
     thresholds = ((global_input.split("\n"))[0]).split(",")
     f_measures = ((global_input.split("\n"))[1]).split(",")
-    
-    # print(f"thresholds= {thresholds}")
-    return (thresholds, f_measures)
 
-class FileLoader (threading.Thread):
+    # print(f"thresholds= {thresholds}")
+    return thresholds, f_measures
+
+
+class FileLoader(threading.Thread):
     def __init__(self, filename, thr_f_measure):
         super(FileLoader, self).__init__()
         self.filename = filename
